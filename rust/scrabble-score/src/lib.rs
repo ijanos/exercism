b87229863ae88c34cbd84fc02822bc89514a4723
János Illés <ijanos@gmail.com>
2016-06-04 12:24:17 +0200

    solve scrabble-score exercise

---
+++ rust/scrabble-score/src/lib.rs
@@ -0,0 +1,15 @@
+pub fn score(word: &str) -> u32 {
+    let letter_value = |c| match c {
+        'A' | 'E' | 'I' | 'O' | 'U' | 'L' | 'N' | 'R' | 'S' | 'T' => 1,
+        'D' | 'G' => 2,
+        'B' | 'C' | 'M' | 'P' => 3,
+        'F' | 'H' | 'V' | 'W' | 'Y' => 4,
+        'K' => 5,
+        'J' | 'X' => 8,
+        'Q' | 'Z' => 10,
+        _ => 0,
+    };
+    word.to_uppercase().chars()
+        .map(letter_value)
+        .fold(0, |sum, i| sum + i)
+}
